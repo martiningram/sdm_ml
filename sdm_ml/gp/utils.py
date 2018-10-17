cce672eb305658ccd17eedde00594fe14e5c7468
Martin Ingram <ingramm@student.unimelb.edu.au>
2018-10-17 19:32:16 +1100

    Minibatching the K-Means is now optional

--- sdm_ml/gp/utils.py
+++ sdm_ml/gp/utils.py
@@ -1,13 +1,19 @@
 import numpy as np
 from scipy.stats import norm
-from sklearn.cluster import MiniBatchKMeans
+from sklearn.cluster import MiniBatchKMeans, KMeans
 
 
-def find_starting_z(X, num_inducing):
+def find_starting_z(X, num_inducing, use_minibatching=True):
     # Find starting locations for inducing points
-    k_means = MiniBatchKMeans(n_clusters=num_inducing)
+
+    if use_minibatching:
+        k_means = MiniBatchKMeans(n_clusters=num_inducing)
+    else:
+        k_means = KMeans(n_clusters=num_inducing)
+
     k_means.fit(X)
     Z = k_means.cluster_centers_
+
     return Z
 
 
