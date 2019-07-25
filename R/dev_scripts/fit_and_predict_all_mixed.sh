a9acc404717026a39719608029de10c2d9dec5aa
Martin Ingram <ingramm@student.unimelb.edu.au>
2019-07-25 15:10:43 +1000

    Added mixed model fit and predict

---
+++ R/dev_scripts/fit_and_predict_all_mixed.sh
@@ -0,0 +1,21 @@
+dataset_dir="./datasets"
+
+for subdir in "$dataset_dir"/*; do
+
+  echo "Fitting and predicting $subdir ..."
+
+  cur_train_x="$subdir/x_train.csv"
+  cur_train_y="$subdir/y_train.csv"
+  cur_test_x="$subdir/x_test.csv"
+  cur_target_dir="$subdir"
+
+  Rscript fit_and_predict_mixed.R \
+    --train-x-csv "$cur_train_x" \
+    --train-y-csv "$cur_train_y" \
+    --test-x-csv "$cur_test_x" \
+    --target-dir "$cur_target_dir" \
+    --test-run
+
+  break
+
+done
