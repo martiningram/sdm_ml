0ae734abe7996de51d4f92989b8815e6ec72cbbb
Martin Ingram <ingramm@student.unimelb.edu.au>
2019-07-25 13:54:18 +1000

    Added R scripts to fit BRT

---
+++ R/brt_fit.R
@@ -0,0 +1,40 @@
+library(dismo)
+library(gbm)
+
+# From Nick's code
+# predict from a BRT model
+brtFit <- function(y, x.train) {
+
+  # We're passing in a matrix because that's easier from python
+  x.train <- data.frame(x.train)
+  
+  # fit a BRT with 10,000 trees and Jane Elith's defaults otherwise
+  m <- gbm(y ~ .,
+           distribution = 'bernoulli',
+           data = x.train,
+           interaction.depth = 5,
+           shrinkage = 0.001,
+           n.trees = 10000,
+           cv.folds = 5,
+           bag.fraction = 0.5,
+           verbose = FALSE,
+           n.cores = 1)
+  
+  ntree <- gbm.perf(m,
+                    plot.it = FALSE,
+                    method = 'cv')
+
+  return (list(ntree=ntree, m=m))
+  
+}
+
+
+brtPredict <- function(model, x.test, ntree) {
+
+  x.test <- data.frame(x.test)
+
+  p <- predict(model, x.test, n.tree = ntree, type = 'response')
+
+  return (p)
+
+}
---
+++ R/dev_scripts/fit_and_predict_all_brt.sh
@@ -0,0 +1,18 @@
+dataset_dir="./datasets"
+
+for subdir in "$dataset_dir"/*; do
+
+  echo "Fitting and predicting $subdir ..."
+
+  cur_train_x="$subdir/x_train.csv"
+  cur_train_y="$subdir/y_train.csv"
+  cur_test_x="$subdir/x_test.csv"
+  cur_target_dir="$subdir"
+
+  Rscript fit_and_predict_brt.R \
+    --train-x-csv "$cur_train_x" \
+    --train-y-csv "$cur_train_y" \
+    --test-x-csv "$cur_test_x" \
+    --target-dir "$cur_target_dir"
+
+done
