d958b3bdfa6011e4b73cfa94440588e4eb2a2f13
Martin Ingram <martin.ingram@gmail.com>
2020-10-20 15:23:32 +1100

    Switch SOGP to JAX & change likelihood a bit

--- sdm_ml/checklist_level/likelihoods.py
+++ sdm_ml/checklist_level/likelihoods.py
@@ -3,14 +3,14 @@ from jax.scipy.special import logsumexp
 from jax.nn import log_sigmoid
 
 
-def compute_checklist_likelihood(pres_abs_logit, obs_logit, m, rel_cells, n_cells):
+def compute_checklist_likelihood(pres_abs_logit, obs_logit, m, cell_nums, n_cells):
     # This computes the log probability of observing the data m (the missingness
     # indicator, 1 if no observation, 0 if observation) given the probability of
     # presence in the cell on the logit scale and the probability of observing the
     # species if present on the logit scale.
     # The pres_abs_logit is on the grid cell scale. Since there can be
-    # multiple observations per grid cell, the "rel_cells" array should match
-    # them up. Specifically, entry rel_cells[i] should contain the index j so
+    # multiple observations per grid cell, the "cell_nums" array should match
+    # them up. Specifically, entry cell_nums[i] should contain the index j so
     # that it belongs to the grid cell whose presence probability is in
     # pres_abs_logit[j]. n_cells is the total number of cells.
 
@@ -21,7 +21,7 @@ def compute_checklist_likelihood(pres_abs_logit, obs_logit, m, rel_cells, n_cell
     log_prob_obs_if_pres = log_sigmoid(obs_logit)
 
     rel_log_probs = jnp.where(m == 0, log_prob_obs_if_pres, log_prob_miss_if_pres)
-    summed_liks = jnp.bincount(rel_cells, weights=rel_log_probs, length=n_cells)
+    summed_liks = jnp.bincount(cell_nums, weights=rel_log_probs, length=n_cells)
 
     lik_term_1 = log_prob_abs
     lik_term_2 = log_prob_pres + summed_liks
@@ -31,7 +31,7 @@ def compute_checklist_likelihood(pres_abs_logit, obs_logit, m, rel_cells, n_cell
 
     not_miss = 1 - m
 
-    obs_per_cell = jnp.bincount(rel_cells, weights=not_miss, length=n_cells)
+    obs_per_cell = jnp.bincount(cell_nums, weights=not_miss, length=n_cells)
     log_lik = jnp.where(obs_per_cell == 0, lik_if_all_missing, lik_if_at_least_one_obs)
 
     return log_lik
--- sdm_ml/gp/sogp.py
+++ sdm_ml/gp/sogp.py
@@ -2,17 +2,32 @@ import os
 import pickle
 import numpy as np
 from tqdm import tqdm
-from svgp.tf.models.sogp_classifier import fit, predict_probs
+from svgp.jax.helpers.sogp import (
+    fit_bernoulli_sogp,
+    ard_plus_bias_kernel_currier,
+    gamma_default_lscale_prior_fn,
+)
+from svgp.jax.helpers.svgp_spec import project_to_x
 from sdm_ml.presence_absence_model import PresenceAbsenceModel
 from sklearn.preprocessing import StandardScaler
+from ml_tools.jax_kernels import ard_rbf_kernel, matern_kernel_32
+from functools import partial
+from ml_tools.normals import normal_cdf_integral
 
 
 class SOGP(PresenceAbsenceModel):
-
     def __init__(self, n_inducing, kernel, seed=2):
 
+        assert kernel in [
+            "matern_32",
+            "rbf",
+        ], "Only matern_32 and rbf kernels supported for now!"
+
+        self.base_kernel_fn = (
+            matern_kernel_32 if kernel == "matern_32" else ard_rbf_kernel
+        )
+
         self.n_inducing = n_inducing
-        self.kernel = kernel
         self.seed = seed
 
         self.scaler = None
@@ -21,8 +36,7 @@ class SOGP(PresenceAbsenceModel):
     @classmethod
     def from_fit_result(cls, fit_result, scaler):
 
-        base_object = cls(n_inducing=fit_result.mu.shape[0],
-                          kernel=fit_result.kernel)
+        base_object = cls(n_inducing=fit_result.mu.shape[0], kernel=fit_result.kernel)
 
         base_object.fit_result = fit_result
         base_object.scaler = scaler
@@ -35,9 +49,24 @@ class SOGP(PresenceAbsenceModel):
         self.scaler = StandardScaler()
         X = self.scaler.fit_transform(X)
 
+        init_params = {
+            "log_lengthscales": np.random.uniform(2.0, 4.0, size=X.shape[1]),
+            "log_alpha": np.array(0.0),
+            "log_bias_sd": np.array(0.0),
+        }
+
         self.fit_results = [
-            fit(X, cur_y, self.n_inducing, random_seed=self.seed,
-                kernel=self.kernel)
+            fit_bernoulli_sogp(
+                X=X,
+                y=cur_y,
+                init_params=init_params,
+                kernel_currier=partial(
+                    ard_plus_bias_kernel_currier, base_kernel=self.base_kernel_fn
+                ),
+                prior_fun=gamma_default_lscale_prior_fn,
+                n_inducing=self.n_inducing,
+                random_seed=self.seed,
+            )
             for cur_y in tqdm(y.T)
         ]
 
@@ -45,10 +74,13 @@ class SOGP(PresenceAbsenceModel):
 
         X = self.scaler.transform(X)
 
-        pred_probs = np.stack([
-            predict_probs(cur_fit_result, X)
-            for cur_fit_result in self.fit_results
-        ], axis=1)
+        pred_means_and_vars = [
+            project_to_x(cur_fit_result[0], X) for cur_fit_result in self.fit_results
+        ]
+
+        pred_probs = np.stack(
+            [normal_cdf_integral(x[0], x[1]) for x in pred_means_and_vars], axis=1
+        )
 
         return np.stack([np.log(1 - pred_probs), np.log(pred_probs)], axis=-1)
 
@@ -66,9 +98,10 @@ class SOGP(PresenceAbsenceModel):
 
         # Save the results files
         for i, cur_results in enumerate(self.fit_results):
-            np.savez(os.path.join(target_folder, f'results_file_{i}'),
-                     **cur_results._asdict())
+            np.savez(
+                os.path.join(target_folder, f"results_file_{i}"), **cur_results[1],
+            )
 
         # Save the scaler
-        with open(os.path.join(target_folder, 'scaler.pkl'), 'wb') as f:
+        with open(os.path.join(target_folder, "scaler.pkl"), "wb") as f:
             pickle.dump(self.scaler, f)
