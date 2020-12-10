b83a557cf44c56300f1932958641abe745561d0f
Martin Ingram <martin.ingram@gmail.com>
2020-12-10 02:16:44 +0000

    Added runtime and a few fixes

--- scripts/evaluate_ebird.py
+++ scripts/evaluate_ebird.py
@@ -3,28 +3,29 @@ from sdm_ml.checklist_level.checklist_dataset import (
     load_ebird_dataset,
     get_arrays_for_fitting,
 )
-from sdm_ml.checklist_level.ebird_joint_checklist_model import EBirdJointChecklistModel
+from sdm_ml.checklist_level.ebird_joint_checklist_model_efficient import EBirdJointChecklistModel
 from sdm_ml.dataset import load_bbs_dataset_2019
 from sdm_ml.evaluation import compute_and_save_results_for_evaluation
 from sdm_ml.dataset import SpeciesData
 import pandas as pd
+import time
 
 
 ebird_dataset = load_ebird_dataset(
-    "/home/martin/data/ebird-basic-dataset/scripts/checklists_with_folds.csv",
-    "/home/martin/data/ebird-basic-dataset/scripts/raster_cell_covs.csv",
-    "/home/martin/data/ebird-basic-dataset/june_2019/species_separated/",
+    "/home/martin/data/scripts/checklists_with_folds.csv",
+    "/home/martin/data/scripts/raster_cell_covs.csv",
+    "/home/martin/data/species_separated"
 )
 
-model = EBirdJointChecklistModel()
+model = EBirdJointChecklistModel(env_interactions=False)
 
 bbs_2019 = load_bbs_dataset_2019(
-    "/home/martin/data/ebird-basic-dataset/scripts/bbs_with_folds.csv",
-    "/home/martin/data/ebird-basic-dataset/scripts/raster_cell_covs.csv",
+    "/home/martin/data/scripts/bbs_with_folds.csv",
+    "/home/martin/data/scripts/raster_cell_covs.csv"
 )
 
 # Fetch arrays for one species
-cur_species = "Anas crecca"
+cur_species = "Larus argentatus"
 arrays = get_arrays_for_fitting(ebird_dataset, cur_species)
 X_env_cell = arrays["env_covariates"]
 
@@ -49,8 +50,9 @@ species_names = sorted(
     list(set(ebird_dataset.species_names) & (set(bbs_2019["train"].outcomes.columns)))
 )
 
-species_names = species_names[:4]
+# species_names = species_names[:4]
 
+start_time = time.time()
 model.fit(
     X_env_cell=X_env_cell,
     X_checklist=X_checklist,
@@ -58,6 +60,7 @@ model.fit(
     checklist_cell_ids=checklist_cell_ids,
     species_names=species_names,
 )
+runtime = time.time() - start_time
 
 test_set = bbs_2019["test"]
 
@@ -70,6 +73,9 @@ test_set_dict["covariates"] = pd.DataFrame(
 
 test_set_amended = SpeciesData(**test_set_dict)
 
+target_dir = "./evaluations/hierarchical_checklist_model_advi_rerun_no_inter_no_env_hierarchy/"
+
 compute_and_save_results_for_evaluation(
-    test_set_amended, model, "./evaluations/hierarchical_checklist_model_advi/"
-)
+    test_set_amended, model, target_dir)
+
+print(runtime, file=open(target_dir + 'runtime.txt', "w"))
